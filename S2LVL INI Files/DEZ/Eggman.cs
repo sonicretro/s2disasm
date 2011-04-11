039ccb4fc22cda7cf688b9af95c57dfe56cce578
unknown <devnull@localhost>
2011-04-11 15:35:35 +0000

    MainMemory - S2LVL: Adding definition for Eggman in DEZ.

---
+++ S2LVL INI Files/DEZ/Eggman.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using Extensions;
+using SonicRetro.S2LVL;
+
+namespace S2ObjectDefinitions.DEZ
+{
+    class Eggman : SonicRetro.S2LVL.ObjectDefinition
+    {
+        private Point offset;
+        private Bitmap img;
+        private int imgw, imgh;
+
+        public override void Init(Dictionary<string, string> data)
+        {
+            List<byte> tmpartfile = new List<byte>(new byte[0xA000]);
+            tmpartfile.AddRange(ObjectHelper.OpenArtFile("../art/nemesis/Robotnik's head.bin", Compression.CompressionType.Nemesis));
+            tmpartfile.AddRange(new byte[0xA300 - tmpartfile.Count]);
+            tmpartfile.AddRange(ObjectHelper.OpenArtFile("../art/nemesis/Robotnik.bin", Compression.CompressionType.Nemesis));
+            tmpartfile.AddRange(new byte[0xAC80 - tmpartfile.Count]);
+            tmpartfile.AddRange(ObjectHelper.OpenArtFile("../art/nemesis/Robotnik's lover half.bin", Compression.CompressionType.Nemesis));
+            byte[] artfile = tmpartfile.ToArray();
+            byte[] mapfile = System.IO.File.ReadAllBytes("../mappings/sprite/objC6_a.bin");
+            img = ObjectHelper.MapToBmp(artfile, mapfile, 0, 0, out offset);
+            imgw = img.Width;
+            imgh = img.Height;
+        }
+
+        public override ReadOnlyCollection<byte> Subtypes()
+        {
+            return new ReadOnlyCollection<byte>(new byte[] { 0 });
+        }
+
+        public override string Name()
+        {
+            return "Eggman";
+        }
+
+        public override bool RememberState()
+        {
+            return false;
+        }
+
+        public override string SubtypeName(byte subtype)
+        {
+            return string.Empty;
+        }
+
+        public override string FullName(byte subtype)
+        {
+            return Name();
+        }
+
+        public override Bitmap Image()
+        {
+            return img;
+        }
+
+        public override Bitmap Image(byte subtype)
+        {
+            return img;
+        }
+
+        public override void Draw(Graphics gfx, Point loc, byte subtype, bool XFlip, bool YFlip)
+        {
+            gfx.DrawImageFlipped(img, loc.X + offset.X, loc.Y + offset.Y, XFlip, YFlip);
+        }
+
+        public override Rectangle Bounds(Point loc, byte subtype)
+        {
+            return new Rectangle(loc.X + offset.X, loc.Y + offset.Y, imgw, imgh);
+        }
+
+        public override void PaletteChanged(System.Drawing.Imaging.ColorPalette pal)
+        {
+            img.Palette = pal;
+        }
+
+        public override void DrawExport(BitmapBits bmp, Point loc, byte subtype, bool XFlip, bool YFlip, bool includeDebug)
+        {
+            BitmapBits bits = new BitmapBits(img);
+            bits.Flip(XFlip, YFlip);
+            bmp.DrawBitmapComposited(bits, new Point(loc.X + offset.X, loc.Y + offset.Y));
+        }
+    }
+}
