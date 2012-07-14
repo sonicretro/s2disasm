7bf0b23c3e4c5ac8a7b1de5e8febd017adde864b
marzojr <marzojr@Skyrise>
2012-07-14 13:54:44 -0300

    Fixing build script error.

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ if [[ ! -f bin/fixheader ]]; then
 	g++ -O3 -s -o bin/fixheader build_source/fixheader.cpp &> /dev/null
 fi
 
-[[ -f s2.bin ]] && mv -f s2.bin s2.prev.bin
+[[ -f s2built.bin ]] && mv -f s2built.bin s2built.prev.bin
 rm -f s2.p s2.h s2.log
 
 debug_syms=""
@@ -62,7 +62,7 @@ if [[ -f s2.log ]]; then
 	exit 1
 fi
 
-[[ -f s2.p ]] && bin/s2p2bin s2.p s2.bin s2.h
-[[ -f s2.bin ]] && bin/fixpointer s2.h s2built.bin   off_3A294 MapRUnc_Sonic \$2D 0 4   word_728C_user Obj5F_MapUnc_7240 2 2 1
-[[ -f s2.bin ]] && bin/fixheader s2.bin
+[[ -f s2.p ]] && bin/s2p2bin s2.p s2built.bin s2.h
+[[ -f s2built.bin ]] && bin/fixpointer s2.h s2built.bin   off_3A294 MapRUnc_Sonic \$2D 0 4   word_728C_user Obj5F_MapUnc_7240 2 2 1
+[[ -f s2built.bin ]] && bin/fixheader s2built.bin
 
