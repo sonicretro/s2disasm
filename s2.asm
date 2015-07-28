d7db686b775c84b554a94d696923b43a3ec2ebea
Clownacy <fred.arnold101@googlemail.com>
2015-07-28 15:57:40 +0100

    Adding explanation for zone animation script

--- s2.asm
+++ s2.asm
@@ -82218,6 +82218,17 @@ start:
 zoneanimcount := zoneanimcount + 1
     endm
 
+;    zoneanimdecl -1, ArtUnc_Flowers1, ArtTile_ArtUnc_Flowers1, 6, 2
+;	-1			Global frame duration. If -1, then each frame will use its own duration, instead
+;	ArtUnc_Flowers1		Source address
+;	ArtTile_ArtUnc_Flowers1	Destination VRAM address
+;	6			Number of frames
+;	2			Number of tiles to load into VRAM for each frame
+
+;    dc.b   0,$7F		; Start of the script proper
+;	0			Tile ID of first tile in ArtUnc_Flowers1 to transfer
+;	$7F			Frame duration. Only here if global duration is -1
+
 ; loc_3FF94:
 Animated_EHZ:	zoneanimstart
 	; Flowers
