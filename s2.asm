8a9b38b33b66a3eeafacdbb9f82c9bc964c174fa
Clownacy <fred.arnold101@googlemail.com>
2015-08-20 19:43:43 +0100

    Adding comment on useless PLR list entries

--- s2.asm
+++ s2.asm
@@ -85381,6 +85381,7 @@ PlrList_Wfz1: plrlistheader
 	plreq ArtTile_ArtNem_BreakPanels, ArtNem_BreakPanels
 	plreq ArtTile_ArtNem_WfzScratch, ArtNem_WfzScratch
 	plreq ArtTile_ArtNem_WfzTiltPlatforms, ArtNem_WfzTiltPlatforms
+	; These two are already in the list, so this is redundant
 	plreq ArtTile_ArtNem_Tornado, ArtNem_Tornado
 	plreq ArtTile_ArtNem_Clouds, ArtNem_Clouds
 PlrList_Wfz1_End
