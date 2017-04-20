074d376ed7507c0d34b64eed3bad8b7fae65b7a3
Clownacy <Clownacy@users.noreply.github.com>
2017-04-20 23:49:37 +0100

    Documented "FloorLog_Unk"
    Appears to have once been a file converter that would overwrite the
    collision array on a dev cart. Perhaps Sonic Team's level editor didn't
    export the correct format?

--- s2.asm
+++ s2.asm
@@ -4404,7 +4404,7 @@ Level_TtlCard:
 	jsrto	(loadZoneBlockMaps).l, JmpTo_loadZoneBlockMaps
 	jsr	(loc_402D4).l
 	jsrto	(DrawInitialBG).l, JmpTo_DrawInitialBG
-	jsr	(FloorLog_Unk).l
+	jsr	(ConvertCollisionArray).l
 	bsr.w	LoadCollisionIndexes
 	bsr.w	WaterEffects
 	bsr.w	InitPlayers
@@ -40342,92 +40342,108 @@ loc_1EAE0:
 ; End of function FindWall2
 
 ; ---------------------------------------------------------------------------
-; Unused floor/wall subroutine - logs something to do with collision
+; The subroutine appears to convert the collision array from an unknown
+; 'raw' format to its current format, and write it to ROM, overwritting
+; the original. This doesn't work on standard read-only cartridges, and
+; would instead require a special dev cartridge.
+; This subroutine exists in Sonic 1 as well, but was oddly changed in
+; the S2 Nick Arcade prototype to just handle loading GHZ's collision
+; instead (though it too is dummied out, hence collision being broken).
 ; ---------------------------------------------------------------------------
 
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
-; return_1EAF0:
-FloorLog_Unk:
+; return_1EAF0: FloorLog_Unk:
+ConvertCollisionArray:
 	rts
 ; ---------------------------------------------------------------------------
+	lea	(ColArray).l,a1	; Source location of 'raw' collision array
+	lea	(ColArray).l,a2	; Destinatation of converted collision array (overwrites the original)
+
+	move.w	#$100-1,d3	; Number of blocks in collision array
+.blockLoop:
+	moveq	#16,d5		; Start on the 16th bit (the leftmost pixel)
+
+	move.w	#16-1,d2	; Width of a block in pixels
+.columnLoop:
+	moveq	#0,d4
+
+	; It seems the 'raw' format stored the collision of each pixel in rows.
+	; This block of code changes it from rows to columns, so each word contains
+	; a bit for each pixel in a column.
+	move.w	#16-1,d1	; Height of a block in pixels
+.rowLoop:
+	move.w	(a1)+,d0	; Get row of collision bits
+	lsr.l	d5,d0		; Push the selected bit of this row into the 'eXtend' flag
+	addx.w	d4,d4		; Shift d4 to the left, and insert the selected bit into bit 0
+	dbf	d1,.rowLoop	; Loop for each row of pixels in a block
+
+	move.w	d4,(a2)+	; Store column of collision bits
+	suba.w	#2*16,a1	; Back to the start of the block
+	subq.w	#1,d5		; Get next bit in the row
+	dbf	d2,.columnLoop	; Loop for each column of pixels in a block
+
+	adda.w	#2*16,a1	; Next block
+	dbf	d3,.blockLoop	; Loop for each block in the collision array
+
 	lea	(ColArray).l,a1
-	lea	(ColArray).l,a2
+	lea	(ColArray2).l,a2	; Write converted collision array to location of rotated collison array
+	bsr.s	.convertArrayToStandardFormat
+	lea	(ColArray).l,a1
+	lea	(ColArray).l,a2		; Write converted collision array to location of normal collison array
 
-	; for d3 from 255 to 0
-	move.w	#$FF,d3
--	moveq	#$10,d5
+; loc_1EB46: FloorLog_Unk2:
+.convertArrayToStandardFormat:
+	move.w	#$1000-1,d3	; Size of the collision array
 
-	; for d2 from 15 to 0
-	move.w	#$F,d2
--	moveq	#0,d4
-
-	; for d1 from 15 to 0
+.processCollisionArrayLoop:
+	moveq	#0,d2
 	move.w	#$F,d1
--	move.w	(a1)+,d0
-	lsr.l	d5,d0
-	addx.w	d4,d4
-	dbf	d1,- ; end for d1
+	move.w	(a1)+,d0	; Get current column of collision pixels
+	beq.s	.noCollision	; Branch if there's no collision in this column
+	bmi.s	.topPixelSolid	; Branch if top pixel of collision is solid
 
-	move.w	d4,(a2)+
-	suba.w	#$20,a1
-	subq.w	#1,d5
-	dbf	d2,-- ; end for d2
-
-	adda.w	#$20,a1
-	dbf	d3,--- ; end for d3
-
-	lea	(ColArray).l,a1
-	lea	(ColArray2).l,a2
-	bsr.s	FloorLog_Unk2
-	lea	(ColArray).l,a1
-	lea	(ColArray).l,a2
-
-; End of function FloorLog_Unk
-
-; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-; loc_1EB46:
-FloorLog_Unk2:
-	move.w	#$FFF,d3
-
--	moveq	#0,d2
-	move.w	#$F,d1
-	move.w	(a1)+,d0
-	beq.s	loc_1EB78
-	bmi.s	++
-
--	lsr.w	#1,d0
-	bcc.s	+
+	; Here we count, starting from the bottom, how many pixels tall
+	; the collision in this column is.
+.processColumnLoop1:
+	lsr.w	#1,d0
+	bcc.s	.pixelNotSolid1
 	addq.b	#1,d2
-+	dbf	d1,-
+.pixelNotSolid1:
+	dbf	d1,.processColumnLoop1
 
-	bra.s	loc_1EB7A
+	bra.s	.columnProcessed
 ; ===========================================================================
-+
-	cmpi.w	#-1,d0
-	beq.s	++
+.topPixelSolid:
+	cmpi.w	#$FFFF,d0		; Is entire column solid?
+	beq.s	.entireColumnSolid	; Branch if so
 
--	lsl.w	#1,d0
-	bcc.s	+
+	; Here we count, starting from the top, how many pixels tall
+	; the collision in this column is (the resulting number is negative).
+.processColumnLoop2:
+	lsl.w	#1,d0
+	bcc.s	.pixelNotSolid2
 	subq.b	#1,d2
-+	dbf	d1,-
+.pixelNotSolid2:
+	dbf	d1,.processColumnLoop2
 
-	bra.s	loc_1EB7A
+	bra.s	.columnProcessed
 ; ===========================================================================
-+
-	move.w	#$10,d0
+.entireColumnSolid:
+	move.w	#16,d0
 
-loc_1EB78:
+; loc_1EB78:
+.noCollision:
 	move.w	d0,d2
 
-loc_1EB7A:
-	move.b	d2,(a2)+
-	dbf	d3,---
+; loc_1EB7A:
+.columnProcessed:
+	move.b	d2,(a2)+	; Store column collision height to ROM
+	dbf	d3,.processCollisionArrayLoop
 
 	rts
 
-; End of function FloorLog_Unk2
+; End of function ConvertCollisionArray
 
     if gameRevision<2
 	nop
