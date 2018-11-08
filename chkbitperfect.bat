d9c7080c20cb4fa22125f3a47d009985696156d6
Clownacy <Clownacy@users.noreply.github.com>
2018-11-08 13:36:32 +0000

    Added bit-perfect testing stuff for Mega Play

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -3,9 +3,9 @@
 REM // build the ROM
 call build %1
 
-REM  // run fc against a Sonic 2 Rev 02 ROM, extracted from Sonic Compilation
+REM  // run fc against a Sonic 2 Mega Play ROM
 echo -------------------------------------------------------------
-IF EXIST s2built.bin ( fc /b s2built.bin s2rev02.bin
+IF EXIST s2built.bin ( fc /b s2built.bin s2megaplay.bin
 ) ELSE echo s2built.bin does not exist, probably due to an assembly error
 
 REM // clean up after us
