10f7dfc1d34a59eafd3fc58c65bf939c2d280916
Clownacy <Clownacy@users.noreply.github.com>
2022-04-19 21:28:24 +0100

    Cleanup and fix warnings in fixpointer

--- build_source/fixpointer.c
+++ build_source/fixpointer.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,14 +10,12 @@ void printUsage(void)
 	printf("usage: fixpointer.exe symbolfile.h romname.bin [patchDestLabel ptrTableLabel   tableIndex destOffset count]*\n");
 }
 
-int stringToInt(const char* string)
+long stringToLong(const char* string)
 {
 	if(string[0] == '$')
-		return strtol(string+1,0,16);
-	else if(string[1] == 'x' || string[1] == 'X')
-		return strtol(string,0,16);
+		return strtol(string+1,NULL,16);
 	else
-		return strtol(string,0,10);
+		return strtol(string,NULL,0);
 }
 
 int main(int argc, char *argv[])
@@ -29,7 +28,7 @@ int main(int argc, char *argv[])
 
 	argc--, argv++;
 
-	char* listingfilename = argv[0];
+	const char* listingfilename = argv[0];
 	FILE* listingfile = fopen(listingfilename,"r");
 	if(!listingfile)
 	{
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	char* romfilename = argv[1];
+	const char* romfilename = argv[1];
 	FILE* romfile = fopen(romfilename,"rb+");
 	if(!romfile)
 	{
@@ -49,7 +48,7 @@ int main(int argc, char *argv[])
 	argc-=2, argv+=2;
 
 	bool z80SizeFound = false, z80SizePatchLocFound = false, z80SizePatched = false;
-	int z80Size = 0, z80SizePatchLoc = 0;
+	long z80Size = 0, z80SizePatchLoc = 0;
 
 	while(argc > 0)
 	{
@@ -60,26 +59,26 @@ int main(int argc, char *argv[])
 		}
 		
 		// get the addresses
-		int addrOfDest = 0;
-		int addrOfSource = 0; 
+		long addrOfDest = 0;
+		long addrOfSource = 0;
 		char* labelOfDest = argv[0];
-		char* labelOfSource = argv[1];
+		const char* labelOfSource = argv[1];
 		char line[128];
 		fseek(listingfile,0,SEEK_SET);
 		for(;;)
 		{
-			fgets(line,128,listingfile);
+			fgets(line,sizeof(line),listingfile);
 			if(feof(listingfile) || ferror(listingfile))
 				break;
-			char* entry0 = strstr(line,labelOfDest);
-			char* entry1 = strstr(line,labelOfSource);
-			int destLabelLen = strlen(labelOfDest);
-			int srcLabelLen = strlen(labelOfSource);
+			const char* entry0 = strstr(line,labelOfDest);
+			const char* entry1 = strstr(line,labelOfSource);
+			size_t destLabelLen = strlen(labelOfDest);
+			size_t srcLabelLen = strlen(labelOfSource);
 
 			if(entry0 && strlen(entry0)>destLabelLen && entry0[destLabelLen]==' ')
-				sscanf(entry0+strlen(labelOfDest)," %X",&addrOfDest);
+				sscanf(entry0+strlen(labelOfDest)," %li",&addrOfDest);
 			if(entry1 && strlen(entry1)>srcLabelLen && entry1[srcLabelLen]==' ')
-				sscanf(entry1+strlen(labelOfSource)," %X",&addrOfSource);
+				sscanf(entry1+strlen(labelOfSource)," %li",&addrOfSource);
 
 			if(argc == 5) // if it's the last time through the loop
 			{
@@ -87,18 +86,24 @@ int main(int argc, char *argv[])
 				if(!z80SizeFound)
 				{
 					static const char* label = "comp_z80_size";
-					char* entry2 = strstr(line,label);
-					int labelLen = strlen(label);
+					const char* entry2 = strstr(line,label);
+					size_t labelLen = strlen(label);
 					if(entry2 && strlen(entry2)>labelLen && entry2[labelLen]==' ')
-						sscanf(entry2+strlen(label)," %X",&z80Size), z80SizeFound = true;
+					{
+						sscanf(entry2+strlen(label)," %li",&z80Size);
+						z80SizeFound = true;
+					}
 				}
 				if(!z80SizePatchLocFound)
 				{
 					static const char* label = "movewZ80CompSize";
-					char* entry2 = strstr(line,label);
-					int labelLen = strlen(label);
+					const char* entry2 = strstr(line,label);
+					size_t labelLen = strlen(label);
 					if(entry2 && strlen(entry2)>labelLen && entry2[labelLen]==' ')
-						sscanf(entry2+strlen(label)," %X",&z80SizePatchLoc), z80SizePatchLocFound = true;
+					{
+						sscanf(entry2+strlen(label)," %li",&z80SizePatchLoc);
+						z80SizePatchLocFound = true;
+					}
 				}
 				if(z80SizeFound && z80SizePatchLocFound && !z80SizePatched)
 				{
@@ -123,38 +128,38 @@ int main(int argc, char *argv[])
 
 		if(addrOfSource && addrOfDest)
 		{
-			int animFrame = stringToInt(argv[2]);
-			int destOffset = stringToInt(argv[3]);
-			int count = stringToInt(argv[4]);
+			long animFrame = stringToLong(argv[2]);
+			long destOffset = stringToLong(argv[3]);
+			long count = stringToLong(argv[4]);
 			if(count>32)
 			{
-				printf("warning: count was %d, max is 32\n", count);
+				printf("warning: count was %ld, max is 32\n", count);
 				count = 32;
 			}
 			
-			int addrSrc = addrOfSource+2*animFrame;
-			int addrDst = addrOfDest+destOffset;
+			long addrSrc = addrOfSource+2*animFrame;
+			long addrDst = addrOfDest+destOffset;
 			fseek(romfile,0,SEEK_END);
-			int romsize = ftell(romfile);
+			long romsize = ftell(romfile);
 			if(addrSrc>=romsize || addrDst>=romsize)
 			{
-				printf("warning: addr src $%X or dst $%X > size $%X\n", addrSrc,addrDst,romsize);
+				printf("warning: addr src $%lX or dst $%lX > size $%lX\n", addrSrc,addrDst,romsize);
 			}
 			else
 			{
 				// patch the rom
 				fseek(romfile,addrSrc,SEEK_SET);
-				unsigned char byte[2];
-				unsigned short off[32];
-				for(int i=0;i<count;i++)
+				unsigned int off[32];
+				for(long i=0;i<count;i++)
 				{
-					byte[0] = fgetc(romfile), byte[1] = fgetc(romfile);
-					off[i] = (byte[0] << 8) | byte[1];
+					const unsigned int byte1 = fgetc(romfile);
+					const unsigned int byte2 = fgetc(romfile);
+					off[i] = (byte1 << 8) | byte2;
 				}
 				fseek(romfile,addrDst,SEEK_SET);
-				for(int i=0;i<count;i++)
+				for(long i=0;i<count;i++)
 				{
-					unsigned int ptr = addrOfSource+off[i];
+					unsigned long ptr = addrOfSource+off[i];
 					fputc((ptr&0xFF000000)>>24,romfile);
 					fputc((ptr&0xFF0000)>>16,romfile);
 					fputc((ptr&0xFF00)>>8,romfile);
