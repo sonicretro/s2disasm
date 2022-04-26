bb013be26cea42d2cba29df9e329974bfb5a979a
Clownacy <Clownacy@users.noreply.github.com>
2022-04-26 02:22:11 +0100

    Document unused mystery code
    Is actually Sonic 2 Beta's level layout loader, a 256x256->128x128
    chunk converter, and a chunk de-duplicator. This gives some insight
    into Sonic 2's development, and how the game's files were converted.

--- s2.asm
+++ s2.asm
@@ -18900,23 +18900,37 @@ loadLevelLayout:
 ; End of function loadLevelLayout
 
 ; ===========================================================================
+
+;loadLevelLayout_Sonic1:
+	; This loads level layout data in Sonic 1's format. Curiously, this
+	; function has been changed since Sonic 1: in particular, it repeats
+	; the rows of the source data to fill the rows of the destination
+	; data, which provides some explanation for why so many of Sonic 2's
+	; backgrounds are repeated in their layout data. This repeating is
+	; needed to prevent Hidden Palace Zone's background from disappearing
+	; when the player moves to the left.
+
+	; Clear layout data.
 	lea	(Level_Layout).w,a3
 	move.w	#bytesToLcnt(Level_Layout_End-Level_Layout),d1
 	moveq	#0,d0
-
 -	move.l	d0,(a3)+
 	dbf	d1,-
 
-	lea	(Level_Layout).w,a3
-	moveq	#0,d1
-	bsr.w	sub_E4A2
-	lea	(Level_Layout+$80).w,a3
-	moveq	#2,d1
+	; The rows of the foreground and background layouts are interleaved
+	; in memory. This is done here:
+	lea	(Level_Layout).w,a3	; Foreground.
+	moveq	#0,d1			; Index into 'Off_Level' to get level foreground layout.
+	bsr.w	.loadLayout
+	lea	(Level_Layout+$80).w,a3	; Background.
+	moveq	#2,d1			; Index into 'Off_Level' to get level background layout.
 
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
-
-sub_E4A2:
+; sub_E4A2:
+.loadLayout:
+	; This expects 'Off_Level' to be in the format that it was in
+	; Sonic 1.
 	moveq	#0,d0
 	move.w	(Current_ZoneAndAct).w,d0
 	ror.b	#1,d0
@@ -18925,120 +18939,151 @@ sub_E4A2:
 	lea	(Off_Level).l,a1
 	move.w	(a1,d0.w),d0
 	lea	(a1,d0.l),a1
+
 	moveq	#0,d1
 	move.w	d1,d2
-	move.b	(a1)+,d1
-	move.b	(a1)+,d2
+	move.b	(a1)+,d1	; Layout width.
+	move.b	(a1)+,d2	; Layout height.
 	move.l	d1,d5
 	addq.l	#1,d5
 	moveq	#0,d3
-	move.w	#$80,d3
-	divu.w	d5,d3
-	subq.w	#1,d3
+	move.w	#$80,d3	; Size of layout row in memory.
+	divu.w	d5,d3	; Get how many times to repeat the source row to fill the destination row.
+	subq.w	#1,d3	; Turn into loop counter.
 
--	movea.l	a3,a0
+.nextRow:
+	movea.l	a3,a0
 
 	move.w	d3,d4
--	move.l	a1,-(sp)
+.repeatRow:
+	move.l	a1,-(sp)
 
 	move.w	d1,d0
--	move.b	(a1)+,(a0)+
-	dbf	d0,-
+.nextByte:
+	move.b	(a1)+,(a0)+
+	dbf	d0,.nextByte
 
 	movea.l	(sp)+,a1
-	dbf	d4,--
+	dbf	d4,.repeatRow
 
-	lea	(a1,d5.w),a1
-	lea	$100(a3),a3
-	dbf	d2,---
+	lea	(a1,d5.w),a1	; Next row in source data.
+	lea	$100(a3),a3	; Next row in destination data.
+	dbf	d2,.nextRow
 
 	rts
-; End of function sub_E4A2
+; End of function .loadLayout
 
 ; ===========================================================================
+
+;ConvertChunksFrom256x256To128x128:
+	; This converts Sonic 1-style 256x256 chunks to Sonic 2-style 128x128
+	; chunks.
+
+	; Destination of 128x128 chunks.
 	lea	($FE0000).l,a1
-	lea	($FE0080).l,a2
+	lea	($FE0000+8*8*2).l,a2
+	; Source of 256x256 chunks.
 	lea	(Chunk_Table).l,a3
 
-	move.w	#$3F,d1
--	bsr.w	sub_E59C
-	bsr.w	sub_E59C
+	move.w	#64-1,d1	; Process 64 256x256 chunks.
+-	bsr.w	ConvertHalfOf256x256ChunkToTwo128x128Chunks
+	bsr.w	ConvertHalfOf256x256ChunkToTwo128x128Chunks
 	dbf	d1,-
 
 	lea	($FE0000).l,a1
 	lea	($FF0000).l,a2
 
-	move.w	#$3F,d1
+	; Insert a blank chunk at the start of chunk table.
+	move.w	#bytesToWcnt(8*8*2),d1
 -	move.w	#0,(a2)+
 	dbf	d1,-
 
-	move.w	#$3FBF,d1
+	; Copy the actual chunks to after this blank chunk.
+	move.w	#bytesToWcnt($8000-(8*8*2)),d1
 -	move.w	(a1)+,(a2)+
 	dbf	d1,-
 
 	rts
 ; ===========================================================================
+
+;EliminateChunkDuplicates:
+	; This is a chunk de-duplicator.
+
+	; Copy first chunk into 'Chunk_Table'.
 	lea	($FE0000).l,a1
 	lea	(Chunk_Table).l,a3
 
-	moveq	#$1F,d0
+	moveq	#bytesToLcnt(8*8*2),d0
 -	move.l	(a1)+,(a3)+
 	dbf	d0,-
 
-	moveq	#0,d7
+	moveq	#0,d7	; This holds how many chunks have been copied minus 1.
 	lea	($FE0000).l,a1
-	move.w	#$FF,d5
-
-loc_E55A:
+	move.w	#$100-1,d5	; $100 chunks
+;loc_E55A:
+.nextChunk:
 	lea	(Chunk_Table).l,a3
 	move.w	d7,d6
 
--	movem.l	a1-a3,-(sp)
-	move.w	#$3F,d0
+.doNextComparison:
+	movem.l	a1-a3,-(sp)
 
+	; Compare chunks.
+	move.w	#bytesToWcnt(8*8*2),d0
 -	cmpm.w	(a1)+,(a3)+
 	bne.s	+
 	dbf	d0,-
+
+	; The chunks match.
 	movem.l	(sp)+,a1-a3
-	adda.w	#$80,a1
-	dbf	d5,loc_E55A
+	adda.w	#8*8*2,a1
+	dbf	d5,.nextChunk
 
 	bra.s	++
 ; ===========================================================================
-+	movem.l	(sp)+,a1-a3
-	adda.w	#$80,a3
-	dbf	d6,--
++
+	; No match: check the next chunk.
+	movem.l	(sp)+,a1-a3
+	adda.w	#8*8*2,a3
+	dbf	d6,.doNextComparison
 
-	moveq	#$1F,d0
+	; Not a single match.
+
+	; Add this chunk to the output.
+	moveq	#bytesToLcnt(8*8*2),d0
 -	move.l	(a1)+,(a3)+
 	dbf	d0,-
 
-	addq.l	#1,d7
-	dbf	d5,loc_E55A
+	addq.l	#1,d7	; One more chunk has been added.
+	dbf	d5,.nextChunk
 /
 	bra.s	-	; infinite loop
 
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
-
-sub_E59C:
-	moveq	#7,d0
--	move.l	(a3)+,(a1)+
+; sub_E59C:
+ConvertHalfOf256x256ChunkToTwo128x128Chunks:
+	moveq	#8-1,d0	 ; 8 rows.
+-
+	; Do a row of chunk 1 (a chunk is 8 blocks wide and tall).
 	move.l	(a3)+,(a1)+
 	move.l	(a3)+,(a1)+
 	move.l	(a3)+,(a1)+
+	move.l	(a3)+,(a1)+
+	; Do a row of chunk 2.
 	move.l	(a3)+,(a2)+
 	move.l	(a3)+,(a2)+
 	move.l	(a3)+,(a2)+
 	move.l	(a3)+,(a2)+
 	dbf	d0,-
 
-	adda.w	#$80,a1
-	adda.w	#$80,a2
-	rts
-; End of function sub_E59C
+	adda.w	#8*8*2,a1
+	adda.w	#8*8*2,a2
 
- ; ===========================================================================
+	rts
+; End of function ConvertHalfOf256x256ChunkToTwo128x128Chunks
+
+; ===========================================================================
 
     if gameRevision=0
 	nop
