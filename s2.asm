4c497075de0c716f27c4cfdcc9d72be6330f788e
Clownacy <Clownacy@users.noreply.github.com>
2022-05-06 21:20:24 +0100

    Clean up PalCycle_SuperSonic and fix a bug

--- s2.asm
+++ s2.asm
@@ -2890,15 +2890,15 @@ CyclingPal_WFZ2:
 ; sub_213E:
 PalCycle_SuperSonic:
 	move.b	(Super_Sonic_palette).w,d0
-	beq.s	++	; rts	; return, if Sonic isn't super
-	bmi.w	PalCycle_SuperSonic_normal	; branch, if fade-in is done
+	beq.s	.return	; return, if Sonic isn't super
+	bmi.w	.normal	; branch, if fade-in is done
 	subq.b	#1,d0
-	bne.s	PalCycle_SuperSonic_revert	; branch for values greater than 1
+	bne.s	.revert	; branch for values greater than 1
 
 	; fade from Sonic's to Super Sonic's palette
 	; run frame timer
 	subq.b	#1,(Palette_timer).w
-	bpl.s	++	; rts
+	bpl.s	.return
 	move.b	#3,(Palette_timer).w
 
 	; increment palette frame and update Sonic's palette
@@ -2913,15 +2913,30 @@ PalCycle_SuperSonic:
 	lea	(Normal_palette+4).w,a1
 	move.l	(a0,d0.w),(a1)+
 	move.l	4(a0,d0.w),(a1)
-	; note: the fade in for Sonic's underwater palette is missing.
-	; branch to the code below (*) to fix this
-/	rts
+    if fixBugs
+	; underwater palettes
+	lea	(CyclingPal_CPZUWTransformation).l,a0
+	cmpi.b	#chemical_plant_zone,(Current_Zone).w
+	beq.s	+
+	cmpi.b	#aquatic_ruin_zone,(Current_Zone).w
+	bne.s	.return
+	lea	(CyclingPal_ARZUWTransformation).l,a0
++	lea	(Underwater_palette+4).w,a1
+	move.l	(a0,d0.w),(a1)+
+	move.l	4(a0,d0.w),(a1)
+    else
+	; Note: The fade in for Sonic's underwater palette is missing.
+	; Because of this, Super Sonic's transformation will be uncorrect
+	; when underwater.
+    endif
+.return:
+	rts
 ; ===========================================================================
-; loc_2188:
-PalCycle_SuperSonic_revert:	; runs the fade in transition backwards
+; loc_2188: PalCycle_SuperSonic_revert:
+.revert:	; runs the fade in transition backwards
 	; run frame timer
 	subq.b	#1,(Palette_timer).w
-	bpl.s	-	; rts
+	bpl.s	.return
 	move.b	#3,(Palette_timer).w
 
 	; decrement palette frame and update Sonic's palette
@@ -2935,23 +2950,23 @@ PalCycle_SuperSonic_revert:	; runs the fade in transition backwards
 	lea	(Normal_palette+4).w,a1
 	move.l	(a0,d0.w),(a1)+
 	move.l	4(a0,d0.w),(a1)
-	; underwater palettes (*)
+	; underwater palettes
 	lea	(CyclingPal_CPZUWTransformation).l,a0
 	cmpi.b	#chemical_plant_zone,(Current_Zone).w
 	beq.s	+
 	cmpi.b	#aquatic_ruin_zone,(Current_Zone).w
-	bne.s	-	; rts
+	bne.s	.return
 	lea	(CyclingPal_ARZUWTransformation).l,a0
 +	lea	(Underwater_palette+4).w,a1
 	move.l	(a0,d0.w),(a1)+
 	move.l	4(a0,d0.w),(a1)
 	rts
 ; ===========================================================================
-; loc_21E6:
-PalCycle_SuperSonic_normal:
+; loc_21E6: PalCycle_SuperSonic_normal:
+.normal:
 	; run frame timer
 	subq.b	#1,(Palette_timer).w
-	bpl.s	-	; rts
+	bpl.s	.return
 	move.b	#7,(Palette_timer).w
 
 	; increment palette frame and update Sonic's palette
@@ -2970,7 +2985,7 @@ PalCycle_SuperSonic_normal:
 	cmpi.b	#chemical_plant_zone,(Current_Zone).w
 	beq.s	+
 	cmpi.b	#aquatic_ruin_zone,(Current_Zone).w
-	bne.w	-	; rts
+	bne.w	.return
 	lea	(CyclingPal_ARZUWTransformation).l,a0
 +	lea	(Underwater_palette+4).w,a1
 	move.l	(a0,d0.w),(a1)+
