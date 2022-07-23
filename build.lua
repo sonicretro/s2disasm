e0882f8390898cc5f1065ca856f475b1355ec413
Clownacy <Clownacy@users.noreply.github.com>
2022-07-23 20:37:36 +0100

    Highlight the settings portion of the Lua script.

--- build.lua
+++ build.lua
@@ -1,9 +1,17 @@
 #!/usr/bin/env lua
 
+--------------
+-- Settings --
+--------------
+
 -- Set this to true to use a better compression algorithm for the sound driver.
 -- Having this set to false will use an inferior compression algorithm that
 -- results in an accurate ROM being produced.
 local improved_sound_driver_compression = false
+
+---------------------
+-- End of settings --
+---------------------
 
 local function file_exists(path)
 	local file = io.open(path, "rb")
