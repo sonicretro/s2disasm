88743bab112ca01fe75f5493bccb66a6ec38e67f
Clownacy <Clownacy@users.noreply.github.com>
2023-05-14 10:29:29 +0100

    Remove an unused lock-on constant.

--- s2.lockon.asm
+++ s2.lockon.asm
@@ -1,11 +1,9 @@
-; TODO - Temporary.
 TwoPlayerResultsPointers = $87DC
 VsResultsScreen_Act = $87DC
 VsResultsScreen_Zone = $87E4
 VsResultsScreen_Game = $87EC
 VsResultsScreen_SS = $87F4
 VsResultsScreen_SSZone = $87FC
-AniIDTailsAni_Roll = 2
 
     if ~~standaloneKiS2
 ; These are just nonsense.
