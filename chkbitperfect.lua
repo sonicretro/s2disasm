0e2daac032c65e9a4b79def57cc327368cfd8510
Clownacy <Clownacy@users.noreply.github.com>
2023-05-21 09:26:38 +0100

    Make `chkbitperfect.lua` handle build failure.
    This eliminates a runtime error that polutes the console output.

--- chkbitperfect.lua
+++ chkbitperfect.lua
@@ -8,23 +8,25 @@ os.exit = coroutine.yield
 
 -- Build the ROM.
 local co = coroutine.create(function() dofile("build.lua") end)
-assert(coroutine.resume(co))
+local _, exit_code = assert(coroutine.resume(co))
 
 -- Restore os.exit back to normal.
 os.exit = os_exit
 
--- Hash the ROM.
-local hash = clownmd5.HashFile("s2built.bin")
+if exit_code ~= false then
+	-- Hash the ROM.
+	local hash = clownmd5.HashFile("s2built.bin")
 
--- Verify the hash against known builds.
-print "-------------------------------------------------------------"
+	-- Verify the hash against known builds.
+	print "-------------------------------------------------------------"
 
-if hash == "\x8E\x2C\x29\xA1\xE6\x51\x11\xFE\x20\x78\x35\x9E\x68\x5E\x79\x43" then
-	print "ROM is bit-perfect with REV00."
-elseif hash == "\x9F\xEE\xB7\x24\x05\x2C\x39\x98\x2D\x43\x2A\x78\x51\xC9\x8D\x3E" then
-	print "ROM is bit-perfect with REV01."
-elseif hash == "\x11\xD8\xD0\xD1\xD1\x19\xD9\xC7\x31\xBB\xF1\xF3\x03\x2F\xF0\x32" then
-	print "ROM is bit-perfect with REV02 (speculative)."
-else
-	print "ROM is not bit-perfect with REV00, REV01, or REV02."
+	if hash == "\x8E\x2C\x29\xA1\xE6\x51\x11\xFE\x20\x78\x35\x9E\x68\x5E\x79\x43" then
+		print "ROM is bit-perfect with REV00."
+	elseif hash == "\x9F\xEE\xB7\x24\x05\x2C\x39\x98\x2D\x43\x2A\x78\x51\xC9\x8D\x3E" then
+		print "ROM is bit-perfect with REV01."
+	elseif hash == "\x11\xD8\xD0\xD1\xD1\x19\xD9\xC7\x31\xBB\xF1\xF3\x03\x2F\xF0\x32" then
+		print "ROM is bit-perfect with REV02 (speculative)."
+	else
+		print "ROM is not bit-perfect with REV00, REV01, or REV02."
+	end
 end
