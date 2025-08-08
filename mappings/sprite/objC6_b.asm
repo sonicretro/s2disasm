404ab57b6111ee4427c9e3a1c6dbcb90decabac8
AlexField442 <alexfield290@gmail.com>
2025-08-08 16:04:56 +1000

    Revert this mapping change

--- mappings/sprite/objC6_b.asm
+++ mappings/sprite/objC6_b.asm
@@ -5,16 +5,10 @@ Map_objC6_b:	mappingsTable
 	mappingsTableEntry.w	Map_objC6_b_0056
 
 Map_objC6_b_0008:	spriteHeader
-    if fixBugs
-	spritePiece	-8, -$20, 2, 4, 0, 0, 0, 0, 0
-	spritePiece	-8, 0, 2, 4, 0, 0, 0, 0, 0
-    else
-	; This also uses the wrong door mappings!
 	spritePiece	-8, -$20, 2, 2, 0, 0, 0, 0, 0
 	spritePiece	-8, -$10, 2, 2, 0, 0, 0, 0, 0
 	spritePiece	-8, 0, 2, 2, 0, 0, 0, 0, 0
 	spritePiece	-8, $10, 2, 2, 0, 0, 0, 0, 0
-    endif
 Map_objC6_b_0008_End
 
 Map_objC6_b_002A:	spriteHeader
